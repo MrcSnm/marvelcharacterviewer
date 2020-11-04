8d6908bc3a0b32d6790ddf4f5b0439698a9d060c
Hipreme <msnmancini@hotmail.com>
2020-11-04 15:04:09 -0300

    Removed .history folder

--- .history/buildReleaseSigned_20201104133929.bat
+++
@@ -1,8 +0,0 @@
-::password = android
-if NOT EXIST "release\" MKDIR release
-IF NOT EXIST "release-key.jks" (
-    echo "Generating a new keystore"
-    keytool -genkey -v -keystore release/release-key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias mcv
-)
-gradlew assembleRelease
-apksigner sign --ks release/release-key.jks --out my-app-release.apk my-app-unsigned-aligned.apk
